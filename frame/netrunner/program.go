65daaceca8bc8d2a911f4a70a7ba263479d94637
mangofeet <purpleplasticsloth@gmail.com>
2024-04-17 14:45:08 -0600

    more frame stuff

--- frame/netrunner/program.go
+++ frame/netrunner/program.go
@@ -11,11 +11,35 @@ func DrawFrameProgram(ctx *canvas.Context, card *nrdb.Printing) error {
 
 	ctx.Push()
 	ctx.SetFillColor(bgColor)
-	ctx.MoveTo(0, canvasHeight)
-	ctx.LineTo(canvasWidth-(canvasWidth/4), canvasHeight)
-	ctx.LineTo(canvasWidth-(canvasWidth/4), canvasHeight-(canvasHeight/12))
-	ctx.LineTo(0, canvasHeight-(canvasHeight/12))
-	ctx.Close()
+
+	titleBoxHeight := (canvasHeight / 12)
+
+	titleBoxTop := canvasHeight - (canvasHeight / 32)
+	titleBoxBottom := titleBoxTop - titleBoxHeight
+	titleBoxArcStart := canvasWidth - (canvasWidth / 2)
+	titleBoxRight := canvasWidth - (canvasWidth / 8)
+	titleBoxArcCP1 := titleBoxRight - (canvasWidth / 48)
+
+	costContainerR := titleBoxHeight * 0.667
+	costContainerStart := costContainerR * 0.667
+
+	titlePath := &canvas.Path{}
+	titlePath.MoveTo(0, titleBoxTop)
+
+	titlePath.LineTo(costContainerStart, titleBoxTop)
+
+	titlePath.QuadTo(costContainerStart+costContainerR, titleBoxTop+costContainerR, costContainerStart+(costContainerR*2), titleBoxTop)
+
+	titlePath.LineTo(titleBoxArcStart, titleBoxTop)
+	titlePath.QuadTo(titleBoxArcCP1, titleBoxTop, titleBoxRight, titleBoxBottom)
+
+	titlePath.LineTo(costContainerStart+(costContainerR*2), titleBoxBottom)
+	titlePath.QuadTo(costContainerStart+costContainerR, titleBoxBottom-costContainerR, costContainerStart, titleBoxBottom)
+
+	titlePath.LineTo(0, titleBoxBottom)
+	titlePath.Close()
+
+	ctx.DrawPath(0, 0, titlePath)
 	ctx.Fill()
 
 	ctx.MoveTo(canvasWidth/6, 0)
