a3d5a9b5bcb97d7b507b57b3b2174b4670e2431c
mangofeet <purpleplasticsloth@gmail.com>
2024-04-17 13:22:01 -0600

    put faction color getter into art package

---
+++ art/colors.go
@@ -0,0 +1,65 @@
+package art
+
+import "image/color"
+
+func GetFactionBaseColor(factionID string) color.RGBA {
+
+	switch factionID {
+	case "shaper", "weyland_consortium":
+		return color.RGBA{
+			R: 0x7f,
+			G: 0x9f,
+			B: 0x7f,
+			A: 0xff,
+		}
+
+	case "anarch":
+		return color.RGBA{
+			R: 0xdf,
+			G: 0xaf,
+			B: 0x8f,
+			A: 0xff,
+		}
+
+	case "criminal":
+		return color.RGBA{
+			R: 0x8c,
+			G: 0xd0,
+			B: 0xd3,
+			A: 0xff,
+		}
+
+	case "nbn":
+		return color.RGBA{
+			R: 0xf0,
+			G: 0xdf,
+			B: 0xaf,
+			A: 0xff,
+		}
+
+	case "jinteki":
+		return color.RGBA{
+			R: 0xcc,
+			G: 0x93,
+			B: 0x96,
+			A: 0xff,
+		}
+
+	case "haas_bioroid":
+		return color.RGBA{
+			R: 0xc0,
+			G: 0xbe,
+			B: 0xd1,
+			A: 0xff,
+		}
+
+	}
+
+	return color.RGBA{
+		R: 0xee,
+		G: 0xee,
+		B: 0xee,
+		A: 0xff,
+	}
+
+}
--- art/netspace/draw.go
+++ art/netspace/draw.go
@@ -32,7 +32,7 @@ func Draw(ctx *canvas.Context, card *nrdb.Printing) error {
 		startY = prng.Next(seed, int64(canvasHeight/4)) + (int64(canvasHeight / 6))
 	}
 
-	baseColor := getFactionBaseColor(card.Attributes.FactionID)
+	baseColor := art.GetFactionBaseColor(card.Attributes.FactionID)
 
 	cardBGColor := color.RGBA{
 		R: uint8(math.Max(0, math.Min(float64(int64(baseColor.R)-96), 255))),
@@ -117,66 +117,4 @@ func Draw(ctx *canvas.Context, card *nrdb.Printing) error {
 	}
 
 	return nil
-}
-
-func getFactionBaseColor(factionID string) color.RGBA {
-
-	switch factionID {
-	case "shaper", "weyland_consortium":
-		return color.RGBA{
-			R: 0x7f,
-			G: 0x9f,
-			B: 0x7f,
-			A: 0xff,
-		}
-
-	case "anarch":
-		return color.RGBA{
-			R: 0xdf,
-			G: 0xaf,
-			B: 0x8f,
-			A: 0xff,
-		}
-
-	case "criminal":
-		return color.RGBA{
-			R: 0x8c,
-			G: 0xd0,
-			B: 0xd3,
-			A: 0xff,
-		}
-
-	case "nbn":
-		return color.RGBA{
-			R: 0xf0,
-			G: 0xdf,
-			B: 0xaf,
-			A: 0xff,
-		}
-
-	case "jinteki":
-		return color.RGBA{
-			R: 0xcc,
-			G: 0x93,
-			B: 0x96,
-			A: 0xff,
-		}
-
-	case "haas_bioroid":
-		return color.RGBA{
-			R: 0xc0,
-			G: 0xbe,
-			B: 0xd1,
-			A: 0xff,
-		}
-
-	}
-
-	return color.RGBA{
-		R: 0xee,
-		G: 0xee,
-		B: 0xee,
-		A: 0xff,
-	}
-
 }
