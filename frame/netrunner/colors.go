aa3f0eb72dd6a3e9de8afb5a85c7235059f73349
mangofeet <purpleplasticsloth@gmail.com>
2024-04-17 14:02:57 -0600

    some frame cleanup

---
+++ frame/netrunner/colors.go
@@ -0,0 +1,19 @@
+package netrunner
+
+import (
+	"image/color"
+)
+
+var bgColor = color.RGBA{
+	R: 0x1c,
+	G: 0x1c,
+	B: 0x1c,
+	A: 0xcc,
+}
+
+// var textColor = color.RGBA{
+// 	R: 0xdc,
+// 	G: 0xdc,
+// 	B: 0xcc,
+// 	A: 0xff,
+// }
--- frame/netrunner/program.go
+++ frame/netrunner/program.go
@@ -1,27 +1,11 @@
 package netrunner
 
 import (
-	"image/color"
-
 	"github.com/mangofeet/nrdb-go"
 	"github.com/tdewolff/canvas"
 )
 
 func DrawFrameProgram(ctx *canvas.Context, card *nrdb.Printing) error {
-
-	bgColor := color.RGBA{
-		R: 0x1c,
-		G: 0x1c,
-		B: 0x1c,
-		A: 0xcc,
-	}
-
-	// textColor := color.RGBA{
-	// 	R: 0xdc,
-	// 	G: 0xdc,
-	// 	B: 0xcc,
-	// 	A: 0xff,
-	// }
 
 	canvasWidth, canvasHeight := ctx.Size()
 
