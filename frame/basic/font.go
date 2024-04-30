ccc848f0f3d7dfe90a253eb1ba90921bd4c70954
mangofeet <purpleplasticsloth@gmail.com>
2024-04-30 16:30:19 -0600

    add a bit of font fallback stuff

--- frame/basic/font.go
+++ frame/basic/font.go
@@ -1,29 +1,45 @@
 package basic
 
-import "github.com/tdewolff/canvas"
+import (
+	"log"
+
+	"github.com/tdewolff/canvas"
+)
 
 var fontFamily = canvas.NewFontFamily("cardtext")
 
 func init() {
-	if err := fontFamily.LoadSystemFont("Ubuntu", canvas.FontRegular); err != nil {
+
+	if err := loadFont("Ubuntu", "sans-serif", canvas.FontRegular); err != nil {
 		panic(err)
 	}
 
-	if err := fontFamily.LoadSystemFont("Ubuntu", canvas.FontBold); err != nil {
+	if err := loadFont("Ubuntu", "sans-serif", canvas.FontBold); err != nil {
 		panic(err)
 	}
 
-	if err := fontFamily.LoadSystemFont("Ubuntu", canvas.FontItalic); err != nil {
+	if err := loadFont("Ubuntu", "sans-serif", canvas.FontItalic); err != nil {
 		panic(err)
 	}
 
-	if err := fontFamily.LoadSystemFont("UbuntuMono Nerd Font", canvas.FontBlack); err != nil {
+	if err := loadFont("UbuntuMono Nerd Font", "monospace", canvas.FontBlack); err != nil {
 		panic(err)
 	}
 
-	if err := fontFamily.LoadSystemFont("DejaVu Sans", canvas.FontExtraBold); err != nil {
+	if err := loadFont("DejaVu Sans", "monospace", canvas.FontExtraBold); err != nil {
 		panic(err)
 	}
+
+}
+
+func loadFont(name, backup string, style canvas.FontStyle) error {
+	if err := fontFamily.LoadSystemFont(name, style); err != nil {
+		log.Printf(`could not find font "%s", trying "%s"`, name, backup)
+		if err := fontFamily.LoadSystemFont(backup, style); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func getFont(size float64, style canvas.FontStyle) *canvas.FontFace {
