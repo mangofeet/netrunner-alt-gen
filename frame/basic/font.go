74e51409f6be26d01450ad4d9cf6e04af5b0926f
mangofeet <purpleplasticsloth@gmail.com>
2024-05-01 14:46:46 -0600

    embed fonts

--- frame/basic/font.go
+++ frame/basic/font.go
@@ -1,8 +1,10 @@
 package basic
 
 import (
+	"io"
 	"log"
 
+	"github.com/mangofeet/netrunner-alt-gen/assets"
 	"github.com/tdewolff/canvas"
 )
 
@@ -10,33 +12,44 @@ var fontFamily = canvas.NewFontFamily("cardtext")
 
 func init() {
 
-	if err := loadFont("Ubuntu", "sans-serif", canvas.FontRegular); err != nil {
+	if err := loadFont("Ubuntu-R.ttf", "sans-serif", canvas.FontRegular); err != nil {
 		panic(err)
 	}
 
-	if err := loadFont("Ubuntu", "sans-serif", canvas.FontBold); err != nil {
+	if err := loadFont("Ubuntu-B.ttf", "sans-serif", canvas.FontBold); err != nil {
 		panic(err)
 	}
 
-	if err := loadFont("Ubuntu", "sans-serif", canvas.FontItalic); err != nil {
+	if err := loadFont("Ubuntu-RI.ttf", "sans-serif", canvas.FontItalic); err != nil {
 		panic(err)
 	}
 
-	if err := loadFont("Ubuntu Mono", "monospace", canvas.FontBlack); err != nil {
+	if err := loadFont("UbuntuMono-R.ttf", "monospace", canvas.FontBlack); err != nil {
 		panic(err)
 	}
 
 	// the "extra bold" in the font family is used for unicode
 	// symbols, this font seems to be the best at rendering them
-	if err := loadFont("DejaVu Sans", "monospace", canvas.FontExtraBold); err != nil {
+	if err := loadFont("DejaVuSans.ttf", "monospace", canvas.FontExtraBold); err != nil {
 		panic(err)
 	}
 
 }
 
 func loadFont(name, backup string, style canvas.FontStyle) error {
-	if err := fontFamily.LoadSystemFont(name, style); err != nil {
-		log.Printf(`could not find font "%s", trying "%s"`, name, backup)
+
+	fontFile, err := assets.FS.Open(name)
+	if err != nil {
+		return err
+	}
+
+	fontFileBytes, err := io.ReadAll(fontFile)
+	if err != nil {
+		return err
+	}
+
+	if err := fontFamily.LoadFont(fontFileBytes, 0, style); err != nil {
+		log.Printf(`could not load font "%s", trying "%s"`, name, backup)
 		if err := fontFamily.LoadSystemFont(backup, style); err != nil {
 			return err
 		}
